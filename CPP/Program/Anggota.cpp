8205cc5994a83a83f7ae016f0e97229b0cb35f5a
radityadhaaa <radityadhaa2@gmail.com>
2024-02-14 11:35:38 +0700

    Revert "add2"
    This reverts commit 55582fe8a660c7223d142ba029a13505419f10d4.

--- CPP/Program/Anggota.cpp
+++ CPP/Program/Anggota.cpp
@@ -4,28 +4,23 @@ DPBO untuk keberkahanNya maka saya tidak
 melakukan kecurangan seperti yang telah dispesifikasikan. Aamiin.
 */
 
-// Mengimpor library yang diperlukan
 #include <iostream>
 #include <string>
 #include <stdlib.h>
 
 using namespace std;
 
-// Deklarasi kelas Anggota
 class Anggota
 {
 private:
-    // Mendeklarasikan variabel-variabel private
     int dataId;
     string nama;
     string namaBidang;
     string namaPartai;
 
 public:
-    // Deklarasi fungsi masukanData
     void masukanData(Anggota *p, int n)
     {
-        // Meminta pengguna untuk melakukan pemasukan data
         cout << "Masukkan ID: ";
         cin >> p[n].dataId;
         cout << "Masukkan Nama: ";
@@ -38,45 +33,34 @@ public:
         n++;
     }
 
-    // Deklarasi fungsi getId
     int getId()
     {
-        // Mengembalikan nilai dataId
         return this->dataId;
     }
 
-    // Deklarasi fungsi getNama
     string getNama()
     {
-        // Mengembalikan nilai nama
         return this->nama;
     }
 
-    // Deklarasi fungsi getNamaBidang
     string getNamaBidang()
     {
-        // Mengembalikan nilai namaBidang
         return this->namaBidang;
     }
 
-    // Deklarasi fungsi getNamaPartai
     string getNamaPartai()
     {
-        // Mengembalikan nilai namaPartai
         return this->namaPartai;
     }
 
-    // Deklarasi fungsi mengubahData
     void mengubahData(Anggota *p, int idAnggota, int n)
     {
         int ch1;
         int i;
-        // Melakukan iterasi untuk mencari anggota dengan idAnggota
         for (i = 0; i < n; i++)
         {
             if (p[i].dataId == idAnggota)
             {
-                // Meminta pengguna untuk memasukkan data baru
                 cout << "Masukkan Nama yang di Ubah: ";
                 cin >> p[i].nama;
                 cout << "Masukkan Nama Bidang yang di Ubah: ";
@@ -88,19 +72,16 @@ public:
         }
     }
 
-    // Deklarasi fungsi menghapusData
     void menghapusData(Anggota *p, int n, int idAnggota)
     {
         int j = 0;
         int k;
         int flag = 0;
-        // Melakukan iterasi untuk mencari anggota dengan idAnggota
         for (j = 0; j < n; j++)
         {
             if (p[j].dataId == idAnggota)
             {
                 flag = 1;
-                // Menghapus data anggota
                 p[j].dataId = 0;
                 p[j].nama = "";
                 p[j].namaBidang = "";
@@ -108,12 +89,11 @@ public:
                 break;
             }
         }
-        // Menampilkan pesan berdasarkan hasil operasi
-        if (flag == 1) // Kondisi jika flag adalah 1, maka akan menjalankan kondisi dibawah ini
+        if (flag == 1)
         {
             cout << "\nData Berhasil di Hapus\n";
         }
-        else // Kondisi jika flag bukan 1, maka akan menjalankan kondisi dibawah ini
+        else
         {
             cout << "\nData Tidak Ditemukan\n";
         }
--- CPP/Program/Main.cpp
+++ CPP/Program/Main.cpp
@@ -4,25 +4,17 @@ DPBO untuk keberkahanNya maka saya tidak
 melakukan kecurangan seperti yang telah dispesifikasikan. Aamiin.
 */
 
-// Mengimpor library yang diperlukan
 #include <bits/stdc++.h>
-// Mengimpor file Anggota.cpp
 #include "Anggota.cpp"
 
-// Menggunakan namespace std untuk mempersingkat penulisan kode
 using namespace std;
 
-// Mendefinisikan fungsi main
 int main()
 {
-    // Membuat array 'orang' dengan 10 objek Anggota
     Anggota orang[10];
-    // Mendeklarasikan variabel-variabel yang diperlukan
     int i = 0, ch, j, idAnggota;
-    // Mulai loop utama
     while (1)
     {
-        // Menampilkan menu utama
         cout << "\nSelamat Datang di Informasi Anggota DPR";
         cout << "\nSilahkan pilihlah opsi untuk melakukan aksi";
         cout << "\n";
@@ -32,51 +24,38 @@ int main()
         cout << "\n 4. Menampilkan Anggota DPR";
         cout << "\n 5. Keluar";
         cout << "\n";
-        // Menerima input dari pengguna
         cin >> ch;
-        // Melakukan aksi berdasarkan input pengguna
         switch (ch)
         {
         case 1:
-            // Memasukkan data anggota
             orang[0].masukanData(orang, i);
             break;
         case 2:
-            // Meminta ID anggota yang akan dihapus
             cout << "Masukan ID yang akan di Hapus :";
             cin >> idAnggota;
-            // Menghapus data anggota
             orang[0].menghapusData(orang, 10, idAnggota);
             break;
         case 3:
-            // Meminta ID anggota yang akan diperbarui
             cout << "Masukan ID yang akan di Update :";
             cin >> idAnggota;
-            // Memperbarui data anggota
             orang[0].mengubahData(orang, idAnggota, i);
             break;
         case 4:
-            // Menampilkan semua anggota DPR
             cout << "Anggota DPR";
             for (i = 0; i < 10; i++)
             {
-                // Jika nama anggota tidak kosong
                 if (orang[i].getNama() != "")
                 {
-                    // Menampilkan detail anggota
                     cout << "\n" << orang[i].getNama() <<" " << orang[i].getNamaBidang() << " " << orang[i].getNamaPartai() << "\n";
                 } 
             }
             break;
         default:
-            // Menampilkan pesan kesalahan jika input tidak valid
             cout << "Salah, Silahkan masukan inputan sesuai dengan opsi yang tertera";
             break;
         case 5:
-            // Keluar dari program
             exit(0);
         }
     }
-    // Mengembalikan 0 jika program berjalan dengan sukses
     return 0;
 }
